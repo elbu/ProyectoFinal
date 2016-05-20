5918b47507beb2166d9e7a59b74c45db2a8edcf0
fuynfactory <fuynfactory@Gmail.com>
2016-05-20 12:25:53 +0200

    webloggin prmera version

---
+++ ProyectoFinal-war/src/java/Bean/LogginBean.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bean;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author jace
+ */
+@ManagedBean
+@SessionScoped
+public class LogginBean {
+    
+     private boolean message; 
+     private String user;
+     private String pass; 
+     
+    /**
+     * Creates a new instance of NewJSFManagedBean
+     */
+    public LogginBean() {
+    }
+    
+    @PostConstruct
+    public void init() {
+        this.setUser("");
+        this.setPass("");
+        this.setMessage((boolean) Boolean.FALSE);
+    }
+    
+    public String doAction(){
+        
+        this.setUser("");
+        this.setPass("");
+        this.setMessage((boolean) Boolean.TRUE);
+        return "";
+    }
+    
+    public String doRegistrar(){
+        
+        return "";
+    }
+    public String doRecordar(){
+        
+        return "";
+    }
+
+    /**
+     * @return the message
+     */
+    public boolean isMessage() {
+        return message;
+    }
+
+    /**
+     * @param message the message to set
+     */
+    public void setMessage(boolean message) {
+        this.message = message;
+    }
+
+    /**
+     * @return the user
+     */
+    public String getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    /**
+     * @return the pass
+     */
+    public String getPass() {
+        return pass;
+    }
+
+    /**
+     * @param pass the pass to set
+     */
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+}
