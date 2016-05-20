77627a9d89e2ba723dca46db64157155685795e2
marco <marco@Marco>
2016-05-20 12:24:12 +0200

    version 1.0

--- ProyectoFinal-war/src/java/Bean/UsuarioBean.java
+++ ProyectoFinal-war/src/java/Bean/UsuarioBean.java
@@ -6,10 +6,12 @@
 package Bean;
 
 import ejb.UsuarioFacade;
+import entity.Proyecto;
 import entity.Usuario;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 
@@ -24,17 +26,34 @@ public class UsuarioBean implements Serializable {
 
     @EJB
     private UsuarioFacade usuarioFacade;
-
     
+    
+    protected List<Proyecto> listaproyectos;
+    protected List<Proyecto> listaproyectoslider;
     protected Usuario usuariologeado;
     /**
      * Creates a new instance of UsuarioBean
      */
+    
     public UsuarioBean() {
     }
     
     public void logeausuario(){
-        
+        Usuario usuario=usuarioFacade.find(usuariologeado.getIdUsuario());
+        if(usuario!=null){
+            if(usuario.getPassword().equals(usuariologeado.getPassword())){
+            //cargar proyectos y entrar
+            this.cargaProyectos();
+            }else{
+            //contraseña incorrecta
+            }
+        }
+        //usuario no existe 
+    }
+    public void cargaProyectos(){
+        listaproyectos=usuariologeado.getProyectoList();
+        listaproyectoslider=usuariologeado.getProyectoList1();
+        listaproyectos.removeAll(listaproyectoslider);
     }
     
 }
